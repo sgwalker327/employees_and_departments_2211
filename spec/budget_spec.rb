ce00758057e2394b85165151f93e0b94cb830479
Sam Walker <sg.walker327@gmail.com>
2023-01-13 10:37:40 -0700

    I3: Add low_expense_departments and add_departments tests

--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -7,17 +7,33 @@ RSpec.describe Budget do
   let(:customer_service) { Department.new("Customer Service") }
   let(:bobbi) { Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"}) }
   let(:aaron) { Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"}) }
-  
-  describe "#initialize" do
+
+  describe '#initialize' do
     it 'exists and has attributes' do
       expect(soc_budget_2007).to be_a(Budget)
       expect(soc_budget_2007.year).to eq(2007)
     end
   end
 
-  describe '#department_list' do
-    it 'can list all of its departments' do
-      expect(soc_budget_2007.department_list).to eq([customer_service])
+  describe '#add_departments' do
+    it 'can add departments and list them' do
+      soc_budget_2007.add_departments(customer_service)
+      expect(soc_budget_2007.departments).to eq([customer_service])
+    end
+  end
+
+  describe '#low_expense_departments' do
+    it 'can list all of low budget departments' do
+      soc_budget_2007.add_departments(customer_service)
+      customer_service.expense(100)
+      customer_service.expense(25)
+      expect(soc_budget_2007.low_expense_departments).to eq([customer_service])
+    end
+  end
+
+  describe '#employee_salaries' do
+    it 'can return a list of employee_salaries' do
+      
     end
   end
 end
