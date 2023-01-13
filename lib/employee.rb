6876be5302d7cb743799be66f02363dee7d360b0
Sam Walker <sg.walker327@gmail.com>
2023-01-13 09:10:01 -0700

    I1: Complete employee setup w/ attributes

---
+++ lib/employee.rb
@@ -0,0 +1,9 @@
+class Employee
+  attr_reader :name, :age, :salary
+
+  def initialize(info)
+    @name = info[:name]
+    @age = info[:age].to_i
+    @salary = info[:salary].delete('$').to_i
+  end
+end
---
+++ spec/employee_spec.rb
@@ -0,0 +1,14 @@
+require './lib/employee'
+
+RSpec.describe Employee do
+  let(:bobbi) { Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"}) }
+
+  describe '#initialize' do
+	  it 'exists and has attributes' do
+		  expect(bobbi).to be_a(Employee)
+      expect(bobbi.name).to eq("Bobbi Jaeger")
+      expect(bobbi.age).to eq(30)
+      expect(bobbi.salary).to eq(100000)
+    end
+  end
+end
